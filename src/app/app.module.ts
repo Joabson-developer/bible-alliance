56431ebb1b6f9bf63dc8463fd0e19dda06c5209d
Joabson-developer <83815302+Joabson-developer@users.noreply.github.com>
2022-03-11 21:28:48 -0300

    Add component bottom-sheet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { BottomSheetComponent } from './shared/bottom-sheet/bottom-sheet.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    BottomSheetComponent
   ],
   imports: [
     BrowserModule,
