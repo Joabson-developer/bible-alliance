1ed72a19aaad5c723c7a431d55456641aac268f2
Joabson-developer <83815302+Joabson-developer@users.noreply.github.com>
2022-03-11 21:46:04 -0300

    Add others components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { BottomSheetComponent } from './shared/bottom-sheet/bottom-sheet.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { ChapterComponent } from './shared/chapter/chapter.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    BottomSheetComponent
+    BottomSheetComponent,
+    SidebarComponent,
+    ChapterComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
